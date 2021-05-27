9dab96207f04ca1b2b6f9d22e06f1d376b4c969b
dimigeo22 <60009105+dimigeo22@users.noreply.github.com>
2021-05-27 15:55:41 +0300

    Update Driving_Structure.ino

--- Driving/Driving_Structure/Driving_Structure.ino
+++ Driving/Driving_Structure/Driving_Structure.ino
@@ -1,14 +1,28 @@
 #include "rover_navigation.h"
+#include "optical_sensor.h"
+#include <movingAvg.h>
 
+movingAvg Vb_average(1000);
 void setup() {
   Serial.begin(115200);
    
   rover.Init();
+  optical.Init();
+  Vb_average.begin();
+  rover.SetState(HIGH,LOW);
   
 }
+bool c = true;
 
 void loop() {
-  rover.VoltageRegulationStep();
-  rover.move_forward(1,10);
-  
+    
+    rover.VoltageRegulationStep();
+    if(c)
+    {
+      rover.rotate_clockwise(90);
+      rover.move_forward(2.5,200);
+    }
+    
+    c=false;
+   
 }
