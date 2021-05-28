8e5a94ed006c12e468a904cb23a53edc4193491c
HyunjoonJeon <hyunjoon.jeon19@imperial.ac.uk>
2021-05-28 02:09:33 +0200

    added components for the command page and connected the react js website with the node js web server

--- Command/react-website/src/components/Button.js
+++ Command/react-website/src/components/Button.js
@@ -6,15 +6,15 @@ const STYLES = ['btn--primary', 'btn--outline'];
 
 const SIZES = ['btn--medium', 'btn--large'];
 
-export const Button = ({children, type, onClick, buttonStyle, buttonSize}) => {
+export const Button = ({children, type, onClick, buttonStyle, buttonSize, name, value}) => {
     const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
     return (
-        <Link to='/sign-up' className='btn-mobile'>
-            <button className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+        //<Link to='/sign-up' className='btn-mobile'>
+            <button type={type} name={name} value={value} className={`btn ${checkButtonStyle} ${checkButtonSize}`}
             onClick={onClick}>
                 {children}
             </button>
-        </Link>
+        //</Link>
     );
 };
--- Command/react-website/src/components/CommandSection.js
+++ Command/react-website/src/components/CommandSection.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../App.css';
 import {Button} from './Button';
 import './CommandSection.css';
@@ -9,15 +9,17 @@ function CommandSection() {
             <h1>Welcome to the Command page</h1>
             <h3>Connection status: </h3>
             <div className='command-btns'>
-                <Button className='btns' buttonStyle='btn-outline' buttonSize='btn--medium'>
-                    Command 1
-                </Button>
-                <Button className='btns' buttonStyle='btn-outline' buttonSize='btn--medium'>
-                    Command 2
-                </Button>
-                <Button className='btns' buttonStyle='btn-outline' buttonSize='btn--medium'>
-                    Command 3
-                </Button>
+                <form method="post" className="form">
+                    <Button type="submit" name="button" value="1" className='btns' buttonStyle='btn-outline' buttonSize='btn--medium'>
+                        Command 1
+                    </Button>
+                    <Button type="submit" name="button" value="2" className='btns' buttonStyle='btn-outline' buttonSize='btn--medium'>
+                        Command 2
+                    </Button>
+                    <Button type="submit" name="button" value="3" className='btns' buttonStyle='btn-outline' buttonSize='btn--medium'>
+                        Command 3
+                    </Button>
+                </form>
             </div>
         </div>
     );
